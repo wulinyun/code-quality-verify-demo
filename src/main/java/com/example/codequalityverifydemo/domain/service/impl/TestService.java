596277c9d481104b5335b12a8c1b6e93eca3e0a9
ryan.miao <ryan.miao@nf-3.com>
2019-05-27 16:56:08 +0800

    添加错误的checkstyle demo

--- src/main/java/com/example/codequalityverifydemo/domain/service/impl/TestService.java
+++ src/main/java/com/example/codequalityverifydemo/domain/service/impl/TestService.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestService implements ITestService {
 
+    private Integer age;
+
     public String print(String msg) {
       System.out.println(msg);
+      if (age<0 || age > 100){
+          System.out.println("年龄写错啦");
+      }
         return msg;
     }
 }
